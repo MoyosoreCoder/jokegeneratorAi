d2d78299ddb0c6c5e9f65dd00164223fecdcd79a
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-04-07 19:02:01 +0100

    added event listener in the script.js file

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-let button = document.querySelector('button')
-let jokeDiv = document.querySelector('#joke')
+button = document.querySelector('button');
+button.addEventListener('click', aiGenerateJoke);
 
-function handleButtonClick() {
-    console.log('button clicked')
-    alert('welcome to the joke generator ai/api')
-    jokeDiv.innerHTML = 'loading joke, please wait...'
-    //joke api
-    try{
-        let result = fetch('https://v2.jokeapi.dev/joke/Any?type=single')
-        .then(res =>
-            res.json())
-            .then(data=>{
-                console.log(data)
-                // tell me a joke
-                jokeDiv.innerHTML =  data.joke
-                
-            })
-            
-    } catch(error) {
-        console.error(error)
-        jokeDiv.innerHTML = 'Error fetching joke 😢'
 
-    }
+function aiGenerateJoke(event) {
+    event.preventDefault();
+    alert('welcome to the ai for generating jokes');
+    apiUrl = "";
+    //display joke function
 }
-
-button.addEventListener('click', handleButtonClick)
