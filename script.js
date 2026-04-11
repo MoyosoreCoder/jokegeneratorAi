387a2c5e31ebe719ab796b061c55ac47ace0dcf3
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-04-11 17:10:02 +0100

    used the axios for get request

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-button = document.querySelector('button');
-button.addEventListener('click', aiGenerateJoke);
+  new Typewriter('h1', {
+  strings: 'welcome to mariamdev001 Ai',
+  autoStart: true,
+  cursor: "",
+  delay: 100,
+})
 
+function displayJoke(response){
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.innerHTML = response.data.answer
+  console.log(response.data.answer)
 
-function aiGenerateJoke(event) {
-    event.preventDefault();
-    alert('welcome to the ai for generating jokes');
-    apiUrl = "";
-    //display joke function
 }
+
+
+function aiGenerateJoke() {
+  alert("welcome")
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t"
+  let context = "Your are a funny AI Assistant that tells short and sweet jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
+  let prompt = "Tell me a unique English joke"    
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let jokeElement = document.querySelector("#joke");
+
+  // ✅ show loading BEFORE request
+  jokeElement.innerHTML = "Generating a joke...";
+  axios.get(apiUrl).then(displayJoke)
+  
+  console.log("Called api url")
+
+}
+let button = document.querySelector('button');
+button.addEventListener('click', aiGenerateJoke); 
